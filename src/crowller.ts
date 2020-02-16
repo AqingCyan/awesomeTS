712f602ec0e399515eb457158c26aaaa7a346e61
AqingCyan <aqingcyan@cyanthing.com>
2020-02-16 17:46:49 +0800

    MOD: 区分职责，优化代码

--- src/crowller.ts
+++ src/crowller.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+import path from 'path'
 import superagent from 'superagent'
 import cheerio from 'cheerio'
 
@@ -6,9 +8,19 @@ interface Course {
   count: number
 }
 
+interface CourseResult {
+  time: number
+  data: Course[]
+}
+
+interface Content {
+  [propsName: number]: Course[]
+}
+
 class Crowller {
   private secret = 'secretKey'
   private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
+  private filePath = path.resolve(__dirname, '../data/course.json')
 
   getCourseInfo(html: string) {
     const $ = cheerio.load(html)
@@ -20,20 +32,39 @@ class Crowller {
       const count = parseInt(descs.eq(1).text().split('：')[1], 10)
       courseInfos.push({ title, count })
     })
-    const result = {
+    return {
       time: new Date().getTime(),
       data: courseInfos,
     }
-    console.log(result)
   }
 
   async getRawHtml() {
     const { text } = await superagent.get(this.url)
-    this.getCourseInfo(text)
+    return text
+  }
+
+  generateJsonContent(result: CourseResult) {
+    let fileContent: Content = {}
+    if (fs.existsSync(this.filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+    }
+    fileContent[result.time] = result.data
+    return fileContent
+  }
+
+  writeFile(content: string) {
+    fs.writeFileSync(this.filePath, content)
+  }
+
+  async initSpiderProcess() {
+    const html = await this.getRawHtml()
+    const courseResult = this.getCourseInfo(html)
+    const fileContent = this.generateJsonContent(courseResult)
+    this.writeFile(JSON.stringify(fileContent))
   }
 
   constructor() {
-    this.getRawHtml()
+    this.initSpiderProcess()
   }
 }
 
