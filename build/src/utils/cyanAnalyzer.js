cb659090f18e46aaf3065838df0804e721d1a1b2
AqingCyan <aqingcyan@cyanthing.com>
2020-03-01 00:30:35 +0800

    ADD: 简单装饰器的学习

---
+++ build/src/utils/cyanAnalyzer.js
@@ -0,0 +1,50 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var cheerio_1 = __importDefault(require("cheerio"));
+var fs_1 = __importDefault(require("fs"));
+var CyanAnalyzer = /** @class */ (function () {
+    function CyanAnalyzer() {
+    }
+    // 单例模式改造
+    CyanAnalyzer.getInstance = function () {
+        if (!CyanAnalyzer.instance) {
+            CyanAnalyzer.instance = new CyanAnalyzer();
+        }
+        return CyanAnalyzer.instance;
+    };
+    // 内容抽取
+    CyanAnalyzer.getCourseInfo = function (html) {
+        var $ = cheerio_1.default.load(html);
+        var courseItems = $('.course-item');
+        var courseInfos = [];
+        courseItems.map(function (index, element) {
+            var descs = $(element).find('.course-desc');
+            var title = descs.eq(0).text();
+            var count = parseInt(descs.eq(1).text().split('：')[1], 10);
+            courseInfos.push({ title: title, count: count });
+        });
+        return {
+            time: new Date().getTime(),
+            data: courseInfos,
+        };
+    };
+    // 数据处理成写入的格式
+    CyanAnalyzer.generateJsonContent = function (result, filePath) {
+        var fileContent = {};
+        if (fs_1.default.existsSync(filePath)) {
+            fileContent = JSON.parse(fs_1.default.readFileSync(filePath, 'utf-8'));
+        }
+        fileContent[result.time] = result.data;
+        return fileContent;
+    };
+    CyanAnalyzer.prototype.analyze = function (html, filePath) {
+        var courseInfo = CyanAnalyzer.getCourseInfo(html);
+        var fileContent = CyanAnalyzer.generateJsonContent(courseInfo, filePath);
+        return JSON.stringify(fileContent);
+    };
+    return CyanAnalyzer;
+}());
+exports.default = CyanAnalyzer;
---
+++ learn/simple_decorator.ts
@@ -0,0 +1,26 @@
+// 类的装饰器
+// 装饰器本身就是一个函数
+// 通过@符号使用
+// 装饰器只对类进行修饰，因此，即使不创建实例，装饰器也会执行，创建多个实例，装饰器也只运行一次。它只对类进行修饰，因此，我们可以用它对类进行修改（增强）
+// 类装饰器的参数是类的构造函数
+// 装饰器从下到上，从右到左执行
+
+function testDecorator(flag: boolean) {
+  if (flag) {
+    return function(constructor: any) {
+      constructor.prototype.getName = () => {
+        console.log('decorator')
+      }
+    }
+  } else {
+    return function (constructor: any) {}
+  }
+}
+
+@testDecorator(true)
+class Test {
+
+}
+
+const test: any = new Test()
+
