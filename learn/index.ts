66bb2aa8b6bf43322f487bde7530521afb7ad4c5
AqingCyan <aqingcyan@cyanthing.com>
2020-03-07 18:58:10 +0800

    MOD: 装饰器使用例子

--- learn/index.ts
+++ learn/index.ts
@@ -1,14 +1,31 @@
-// 参数装饰器
-// 第一个参数是原型，第二个是方法信息，第三个是参数的序号
-function paramsDecorator(target: any, method: string, paramIndex: number): any {
-  console.log(target, method, paramIndex)
+// 装饰器妙用
+const userInfo: any = undefined
+
+function catchError(message: string) {
+  return function (target: any, key: string, descriptor: PropertyDescriptor) {
+    const fn = descriptor.value
+    descriptor.value = function () {
+      try {
+        fn()
+      } catch (e) {
+        console.log(`UserInfo 不存在 ${message} 属性`)
+      }
+    }
+  }
 }
 
 class Test {
-  getInfo(@paramsDecorator name: string, age: number) {
+  @catchError('name')
+  getName() {
+    return userInfo.name
+  }
 
+  getAge() {
+    return userInfo.age
   }
 }
 
 const test = new Test()
-test.getInfo('Aqing', 21)
+test.getName() // undefined 没有 name 属性，但是被装饰器处理过，不会报错
+// test.getAge() // undefined 没有 age 属性，会报错
+
