651c278441937f5a329f66961789502dfb0268a2
AqingCyan <aqingcyan@cyanthing.com>
2020-03-08 22:39:41 +0800

    MOD: 通过装饰器实现路由功能

--- src/controller/loginController.ts
+++ src/controller/loginController.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata'
 import { Request, Response } from 'express'
 import { controller, get } from './decorators'
+import { getResponseData } from "../utils/util"
 
 // 当描述文件不准确时，我们可以拓展它的类型
 interface BodyRequest extends Request{
@@ -9,9 +10,12 @@ interface BodyRequest extends Request{
 
 @controller
 class LoginController {
-  @get('/login')
-  login() {
-
+  @get('/logout')
+  logout(req: BodyRequest, res: Response) {
+    if (req.session) {
+      req.session.login = undefined
+    }
+    res.json(getResponseData(true))
   }
 
   @get('/')
@@ -19,25 +23,24 @@ class LoginController {
       const isLogin = req.session ? req.session.login : false
       if (isLogin) {
         res.send(`
-      <html lang="zh">
-        <body>
-          <a href="/getData">爬取内容</a><br>
-          <a href="/showData">展示内容</a><br>
-          <a href="/logout">退出</a><br>
-        </body>
-      </html>
-    `)
+          <html lang="zh">
+            <body>
+              <a href="/getData">爬取内容</a><br>
+              <a href="/showData">展示内容</a><br>
+              <a href="/logout">退出</a><br>
+            </body>
+          </html>
+        `)
       } else {
         res.send(`
-      <html lang="zh">
-        <body>
-        <form method="post" action="/login">
-          <input type="password" name="password" />
-          <button>提交</button>
-        </form>
-        </body>
-      </html>
-    `)
+          <html lang="zh">
+            <body>
+            <form method="post" action="/login">
+              <input type="password" name="password" />
+              <button>提交</button>
+            </form>
+            </body>
+          </html>`)
     }
   }
 }
