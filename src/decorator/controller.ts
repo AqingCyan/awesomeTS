aff112200d851422d863a375e89b384bee50c8ac
AqingCyan <aqingcyan@cyanthing.com>
2020-03-09 01:12:26 +0800

    MOD: 修改中间件装饰器，可以使用多个中间件装饰器来使用中间件

--- src/decorator/controller.ts
+++ src/decorator/controller.ts
@@ -11,12 +11,13 @@ export const controller = (root: string) => (
     for(let key in target.prototype) {
       const path: string = Reflect.getMetadata('path', target.prototype, key)
       const method: Methods = Reflect.getMetadata('method', target.prototype, key)
-      const middleware: RequestHandler = Reflect.getMetadata('middleware', target.prototype, key)
+      const middlewares: RequestHandler[] = Reflect.getMetadata('middlewares', target.prototype, key)
       const handler: any = target.prototype[key]
       if (path && method) {
         const fullPath = root === '/' ? path : `${root}${path}`
-        if (middleware) {
-          router[method](fullPath, middleware, handler)
+        // 使用多个中间件的情况下
+        if (middlewares && middlewares.length) {
+          router[method](fullPath, ...middlewares, handler)
         } else {
           router[method](fullPath, handler)
         }
