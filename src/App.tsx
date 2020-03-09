cd2ecd0dc74d9867f49dd6794e415e3219ca681e
AqingCyan <aqingcyan@cyanthing.com>
2020-03-09 15:58:45 +0800

    ADD: 前端路由的支持

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,15 @@
-import React, { Component } from 'react'
-import { Form, Icon, Input, Button } from 'antd'
-import { WrappedFormUtils } from 'antd/lib/form/Form'
-import './login.css'
+import React from 'react'
+import { HashRouter , Route, Switch } from 'react-router-dom'
+import LoginPage from './pages/Login'
 
-interface FormFields {
-  password: string
+export default () => {
+  return (
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route path="/login" exact component={LoginPage} />
+        </Switch>
+      </HashRouter>
+    </div>
+  )
 }
-
-interface Props {
-  form: WrappedFormUtils<FormFields>
-}
-
-class LoginForm extends Component<Props> {
-  handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values)
-      }
-    })
-  }
-
-  render() {
-    const { getFieldDecorator } = this.props.form;
-    return (
-      <div className="login-page">
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [{ required: true, message: 'Please input your Password!' }],
-            })(
-              <Input
-                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                type="password"
-                placeholder="Password"
-              />,
-            )}
-          </Form.Item>
-          <Button type="primary" htmlType="submit">
-            Login
-          </Button>
-        </Form>
-      </div>
-    )
-  }
-}
-
-const WrappedLoginForm = Form.create({ name: 'login' })(LoginForm)
-
-export default WrappedLoginForm
